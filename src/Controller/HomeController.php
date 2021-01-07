3f16d1e817f1682af223eba82014dc20da6d9f88
Pulch2021 <pulcherie.begbin@outlook.fr>
2021-01-07 17:02:54 +0100

    persist () et flush()
    persister
    et avancer

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,28 @@
 
 namespace App\Controller;
 
-use stdClass;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use App\Entity\Fruit;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home_page")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $manager): Response
     {
+        // sans injestion de dépendance , donc retirer l'injection
+        // $manager = $this->getDoctrine()->getManager();
+        $fraise = new Fruit();
+        $fraise->setName("fraise");
+
+        $manager->persist($fraise);
+        $manager->flush();
+
 
         return $this->render('home/index.html.twig', []);
     }
