2504b0557ad5995d607978ec45dd6ea92297c23c
Pulch2021 <pulcherie.begbin@outlook.fr>
2021-01-08 10:14:28 +0100

    visualiser ce qui est dans notre base
    home controller : avec la class (fruitRepository)
    index.html.twig, mettrre la vue
    	{{dump(fruits)}}

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 
 use App\Entity\Fruit;
+use App\Repository\FruitRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,26 +15,16 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home_page")
      */
-    public function index(EntityManagerInterface $manager): Response
+    public function index(FruitRepository $repo): Response
     {
-        // sans injection de dépendance , donc retirer l'injection
-        // $manager = $this->getDoctrine()->getManager();
-        $fraise = new Fruit();
-        $ananas = new fruit();
+        $fruits = $repo->findAll();
+
+        dump($fruits);
 
 
-        $fraise->setName("fraise");
-        $ananas->setName("ananas");
+        return $this->render('home/index.html.twig', [
 
-
-        // dit à doctrine on veut sauvegarder l'objet fruit
-        $manager->persist($fraise);
-        $manager->persist($ananas);
-
-        // flush pour tout | exéccute la requête
-        $manager->flush();
-
-
-        return $this->render('home/index.html.twig', []);
+            "fruits" => $fruits
+        ]);
     }
 }
