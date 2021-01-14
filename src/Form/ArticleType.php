a3a23057c29ba8701ceb510786c44a45b5846515
Pulch2021 <pulcherie.begbin@outlook.fr>
2021-01-14 09:43:04 +0100

    tester si ts les champs sont ds le formualire

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -23,14 +23,17 @@ class ArticleType extends AbstractType
             ])
             ->add('intro', TextType::class, [
                 'label' => 'Votre intro',
+                'required' => false,
                 'attr'  => ['placeholder' => 'Une petite phrase d\'acrroche ici']
             ])
             ->add('content', TextareaType::class, [
                 'label' => 'Votre contenu',
+                'required' => false,
                 'attr'  => ['placeholder' => 'Dites-nous tout !']
             ])
             ->add('image', UrlType::class, [
                 'label' => 'Adresse de l\'image',
+                'required' => false,
                 'attr'  => ['placeholder' => 'Coller un lien d\'image']
             ])
             ->add('Envoyer', SubmitType::class);
